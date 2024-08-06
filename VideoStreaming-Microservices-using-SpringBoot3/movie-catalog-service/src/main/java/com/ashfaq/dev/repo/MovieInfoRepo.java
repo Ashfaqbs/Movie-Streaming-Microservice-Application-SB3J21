f180e1ce36eebdf83fca5ef8aa18a9d478ff10b5
Ashfaqbs <ash001x@gmail.com>
2024-08-06 09:04:02 +0530

    General commit

--- VideoStreaming-Microservices-using-SpringBoot3/movie-catalog-service/src/main/java/com/ashfaq/dev/repo/MovieInfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ashfaq.dev.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ashfaq.dev.model.MovieInfo;
-
-public interface MovieInfoRepo  extends JpaRepository<MovieInfo, Long> {
-
-}
